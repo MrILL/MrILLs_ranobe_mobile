efe27fdd09468e3146262a7137975d5d0dd95218
MrILL <ilua2403@ukr.net>
2021-08-12 00:14:00 +0300

    feat: init issues list

--- src/components/MyText.js
+++ src/components/MyText.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
-export function MyText({ style, children }) {
-  return <Text style={[styles.default, style]}>{children}</Text>;
+export function MyText(params) {
+  const { style } = params;
+
+  return <Text {...params} style={[styles.default, style]} />;
 }
 
 const styles = StyleSheet.create({
--- src/navigation/RanobeIssueListScreen.js
+++ src/navigation/RanobeIssueListScreen.js
@@ -1,11 +1,43 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 import { MyText } from "../components/MyText";
+
+const chapters = Array.from({ length: 20 }, (_, i) => {
+  return {
+    id: i,
+    nomer: i,
+    title: `${28 - Math.floor(i / 10)} - ${
+      187 - i
+    } Юпитер / Jupiter, the Bringer of Jollity`,
+    publishData:
+      i < 2
+        ? "Сегодня"
+        : i < 7
+        ? "Вчера"
+        : i < 13
+        ? "3 дня назад"
+        : "21.04.2021",
+  };
+});
 
 export function RanobeIssueListScreen({ ranobeId }) {
   return (
-    <View style={styles.container}>
-      <MyText>suckama!</MyText>
+    <View style={[styles.container, { paddingTop: 2 }]}>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+        {chapters.map((v) => {
+          return (
+            <View style={styles.line_container}>
+              <View style={styles.title_container}>
+                <MyText numberOfLines={2}>{v.title}</MyText>
+              </View>
+
+              <View style={styles.date_container}>
+                <MyText style={styles.date_text}>{v.publishData}</MyText>
+              </View>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -14,5 +46,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFE1CC",
+  },
+
+  line_container: {
+    flex: 1,
+    flexDirection: "row",
+    padding: 12,
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+  },
+
+  title_container: {
+    flex: 6,
+  },
+
+  date_container: {
+    flex: 2,
+    marginLeft: 8,
+    justifyContent: "center",
+  },
+
+  date_text: {
+    textAlign: "center",
   },
 });
